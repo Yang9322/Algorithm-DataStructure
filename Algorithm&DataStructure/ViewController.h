1b85207fd4844dd8cbc5eb72490fe231e47747a7
Yang9322 <576421791@qq.com>
2017-04-08 14:30:18 +0800

    Initial Commit

---
+++ Algorithm&DataStructure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorithm&DataStructure
+//
+//  Created by He yang on 2017/4/8.
+//  Copyright © 2017年 He yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Algorithm&DataStructure/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Algorithm&DataStructure
+//
+//  Created by He yang on 2017/4/8.
+//  Copyright © 2017年 He yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
